8e84a53271046d49da3c74f79a15eab9f3408d00
Maxence 'Linutux' POULAIN <linutux42@users.noreply.github.com>
2020-07-08 16:32:26 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM debian:buster-slim
+
+COPY entrypoint.sh /
+
+RUN apt-get update \
+  && apt-get install -y curl \
+  && rm -rf /var/lib/apt/lists/*
+
+ENV GANDI_DOMAIN ""
+ENV GANDI_RECORD ""
+ENV GANDI_APIKEY ""
+
+ENTRYPOINT [ "/bin/bash", "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IP_SERVICE="http://me.gandi.net"
+GANDI_API="https://api.gandi.net/v5/livedns/domains"
+
+IP4=$(curl -s4 $IP_SERVICE)
+
+if [[ -z "$IP4" ]]; then
+  echo "Something went wrong. Can not get your IP from $IP_SERVICE "
+  exit 1
+fi
+
+if [[ ! -z "$IP4" ]]; then
+  DATA='{"rrset_type": "A", "rrset_values": ["'$IP4'"]}'
+  curl -s -XPUT -d "$DATA" \
+    -H "Authorization: Apikey $GANDI_APIKEY" \
+    -H "Content-Type: application/json" \
+    "${GANDI_API}/${GANDI_DOMAIN}/records/${GANDI_RECORD}/A"
+  exit $?
+fi
+
+exit 1
